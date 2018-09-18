4417e8956a8356d21d3ee06a8b19ae1a3dc8530b
Marco <marcoapefe@hotmail.com>
2018-09-18 23:29:58 +0200

    Refactorización de obtener lista de agricultores.

--- src/main/java/com/proyecto/cooperativa/repositories/FarmerRepository.java
+++ src/main/java/com/proyecto/cooperativa/repositories/FarmerRepository.java
@@ -20,6 +20,7 @@ public class FarmerRepository {
     private static final String LIKE = " LIKE ?";
     private static final String OR = " OR ";
     private static final String PERCENT = "%";
+    private static final String COMMA_SEPARATOR = ",";
     private List<String> farmerFieldsToGet = Arrays.asList("n_socio", "cif_nif",
             "nombre_razon_social",
             "apellido",
@@ -30,9 +31,9 @@ public class FarmerRepository {
     public Map<String, Object> getFarmersList(String textToSearch) {
         StringBuilder query = buildSelectClause();
         String whereClause = buildWhereClause(textToSearch);
-        if (StringUtils.isEmpty(whereClause)) {
+        if (!StringUtils.isEmpty(whereClause)) {
             query.append(whereClause);
-            return jdbcTemplate.queryForMap(query.toString(), buildParametersOfQuery(textToSearch));
+            return jdbcTemplate.queryForMap(query.toString(), buildWhereClauseParameters(textToSearch));
         } else {
             return jdbcTemplate.queryForMap(query.toString());
         }
@@ -41,7 +42,7 @@ public class FarmerRepository {
 
     private StringBuilder buildSelectClause() {
         return new StringBuilder("SELECT  "
-                + farmerFieldsToGet.stream().collect(Collectors.joining(","))
+                + farmerFieldsToGet.stream().collect(Collectors.joining(COMMA_SEPARATOR))
                 + " FROM PERSONAS"
                 + " JOIN (p.id_persona = a.id_persona) ON "
                 + "agricultores a");
@@ -50,15 +51,16 @@ public class FarmerRepository {
     private String buildWhereClause(String textToSearch) {
         String whereClause = "";
         if (!StringUtils.isEmpty(textToSearch)) {
-            whereClause = WHERE + farmerFieldsToGet
-                    .stream().collect(Collectors.joining(LIKE + OR))
+            whereClause = WHERE
+                    + farmerFieldsToGet.stream()
+                    .collect(Collectors.joining(LIKE + OR))
                     + LIKE;
 
         }
         return whereClause;
     }
 
-    private String[] buildParametersOfQuery(String textToSearch) {
+    private String[] buildWhereClauseParameters(String textToSearch) {
         String[] fieldsToSearch = new String[farmerFieldsToGet.size()];
         Arrays.fill(fieldsToSearch, PERCENT + textToSearch + PERCENT);
         return fieldsToSearch;
